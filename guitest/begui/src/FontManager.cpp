d9f902fdb767ca660718c65f021a12c61617efdf
apanagop <apanagop@3b3da2cc-df40-11dd-a550-c997509ed985>
2009-11-26 06:14:17 +0000

    Important font system updates: -fonts can now spread multiple textures (cache pages) -freetype resources are now released... -multiple fonts can share the same texture page -multiple calls to setFont(..) with the same args dont cause the same font to be loaded multiple times any more...

--- guitest/begui/src/FontManager.cpp
+++ guitest/begui/src/FontManager.cpp
@@ -23,10 +23,17 @@
 
 using namespace begui;
 
-std::vector<Font> FontManager::m_fonts;
-FT_Library	FontManager::m_freetype;
-bool		FontManager::m_ftInitialized = false;
-int			FontManager::m_curFont = -1;
+std::vector<Font*>		FontManager::m_fonts;
+FT_Library				FontManager::m_freetype;
+bool					FontManager::m_ftInitialized = false;
+int						FontManager::m_curFont = -1;
+std::vector<Texture*>	FontManager::m_textureList;
+int						FontManager::m_lastCharRight = 0;
+int						FontManager::m_lastCharTop = 0;
+int						FontManager::m_lastCharBottom = 0;
+std::vector<unsigned char*>	FontManager::m_fontDrawingMem;
+int						FontManager::m_texWidth = 512;
+int						FontManager::m_texHeight = 512;
 
 bool FontManager::initialize()
 {
@@ -41,14 +48,141 @@ bool FontManager::initialize()
 	return true;
 }
 
+void FontManager::clear()
+{
+	// destroy all fonts
+	for (size_t i=0; i<m_fonts.size(); ++i)
+		SAFE_DELETE(m_fonts[i]);
+	m_fonts.clear();
+
+	// destroy all textures
+	for (size_t i=0; i<m_textureList.size(); ++i)
+		SAFE_DELETE(m_textureList[i]);
+	m_textureList.clear();
+	m_lastCharRight = 0;
+	m_lastCharTop = 0;
+	m_lastCharBottom = 0;
+
+	// clear font system memory
+	for (size_t i=0; i<m_fontDrawingMem.size(); ++i)
+		SAFE_DELETE(m_fontDrawingMem[i]);
+	m_fontDrawingMem.clear();
+
+	// close freetype
+	FT_Done_FreeType(m_freetype);
+}
+
 bool FontManager::setFont(const std::string &font_name, int font_size)
 {
-	m_fonts.push_back(Font());
-	if (!m_fonts.back().createFont(font_name, font_size))
-	{
-		m_fonts.pop_back();
-		return false;
+	// check if the font is already loaded
+	for (size_t i=0; i<m_fonts.size(); ++i) {
+		if (m_fonts[i]->getFontFileName() == font_name && m_fonts[i]->getFontSize() == font_size)
+			return true;
 	}
+
+	// create the new font
+	Font *pFont = new Font();
+	if (!pFont->createFont(font_name, font_size))
+		return false;
+	m_fonts.push_back(pFont);
 	m_curFont = (int)m_fonts.size()-1;
+
 	return true;
+}
+
+void FontManager::beginFontCaching()
+{
+	// nothing to do here
+}
+
+Font::Character FontManager::allocCharacterDrawingArea(int width, int height)
+{
+	// TODO: update this method to use a quad-tree to keep track
+	// of the free space in each texture, in order to reduce wasted space.
+
+	Font::Character ref;
+
+	// if we already have some allocated textures, and if the last of
+	// them is still writable (font drawing memory is still allocated),
+	// check if this texture has enough space for this character
+	if (m_textureList.size() > 0 && m_fontDrawingMem.back() != 0)
+	{
+		Texture *pLastTex = m_textureList.back();
+
+		// check if we have enough space in the last texture used
+		if (m_lastCharRight + width + 3 < pLastTex->getWidth())
+		{
+			// fill in the returned image ref
+			ref.m_pTexture = pLastTex;
+			ref.m_top = m_lastCharTop;
+			ref.m_left = m_lastCharRight + 2;
+			ref.m_bottom = ref.m_top + height;
+			ref.m_right = ref.m_left + width;
+			ref.m_pDrawingBuffer = m_fontDrawingMem.back();
+			ref.m_drawingBufferPitch = m_texWidth;
+
+			// advance the last character position
+			m_lastCharRight = ref.m_right;
+			m_lastCharTop = ref.m_top;
+			if (m_lastCharBottom < ref.m_bottom)
+				m_lastCharBottom = ref.m_bottom;	// lastCharBottom is the bottom of the whole "line" (wasted space here)
+
+			return ref;
+		}
+		if (m_lastCharBottom + height + 3 < pLastTex->getHeight())
+		{
+			// go to the next line
+
+			// fill in the returned image ref
+			ref.m_pTexture = pLastTex;
+			ref.m_top = m_lastCharBottom + 2;
+			ref.m_left = 2;
+			ref.m_bottom = ref.m_top + height;
+			ref.m_right = ref.m_left + width;
+			ref.m_pDrawingBuffer = m_fontDrawingMem.back();
+			ref.m_drawingBufferPitch = m_texWidth;
+
+			// advance the last character position
+			m_lastCharRight = ref.m_right;
+			m_lastCharTop = ref.m_top;
+			m_lastCharBottom = ref.m_bottom;
+
+			return ref;
+		}
+	}
+
+	// if we got here, no space was available. Create a new texture
+	Texture *pTex = new Texture();
+	m_textureList.push_back(pTex);
+	// and allocate some system memory to draw on..
+	m_fontDrawingMem.push_back(new unsigned char[m_texWidth * m_texHeight * 4]);
+	memset(m_fontDrawingMem.back(), 0, m_texWidth * m_texHeight * 4);
+	// and reset the drawing position
+	m_lastCharRight = 0;
+	m_lastCharTop = 0;
+	m_lastCharBottom = 0;
+
+	ref.m_pTexture = pTex;
+	ref.m_top = 2;
+	ref.m_left = 2;
+	ref.m_bottom = ref.m_top + height;
+	ref.m_right = ref.m_left + width;
+	ref.m_pDrawingBuffer = m_fontDrawingMem.back();
+	ref.m_drawingBufferPitch = m_texWidth;
+
+	return ref;
+}
+
+void FontManager::endFontCaching()
+{
+	// create all textures, copying data from the corresponding drawing surfaces
+	for (size_t i=0; i<m_textureList.size(); ++i)
+	{
+		// only re-create textures where the font drawing memory contains data
+		if (m_fontDrawingMem[i] == 0)
+			continue;
+
+		// create the texture
+		m_textureList[i]->create(m_texWidth, m_texHeight, GL_RGBA, m_fontDrawingMem[i]);
+	}
 }
